8218a350933d23d4c3c5b97dc77f8967bdfbc817
Danila <kimoror@mail.ru>
2021-03-09 22:36:17 +0300

    create table product

--- src/main/backend/java/bookshelf/database/db_creator.sql
+++ src/main/backend/java/bookshelf/database/db_creator.sql
@@ -1,6 +1,16 @@
-drop schema if exists education CASCADE;
+drop schema if exists bookshelf CASCADE;
 
 --Создаём схему
-create schema education;
+create schema bookshelf;
 --Подтверждаем транзакцию
+
+create table bookshelf.product(
+     id int not null PRIMARY KEY,
+     name varchar(100),
+     cost int,
+     img_path text,
+     short_desctiption text,
+     maker_id int,
+     product_type int
+);
 commit;
