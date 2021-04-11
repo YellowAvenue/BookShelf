2042ee8ced37e6e33cb00e3349251d336562a25f
Данила <kimoror@mail.ru>
2021-04-12 01:36:23 +0300

    add products_in_order entity, repository and service

--- src/main/backend/java/bookshelf/models/entities/ProductInOrder.java
+++ src/main/backend/java/bookshelf/models/entities/ProductInOrder.java
@@ -4,23 +4,24 @@ import lombok.*;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
-import java.sql.Date;
-import java.util.List;
 
 @Component
 @Entity
-@Table( name = "orders")
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
-public class Order {
+@Table(name = "products_in_order")
+public class ProductInOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id")
-    private User user;
-    private Date orders_time;
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private int num_of_product;
 }
